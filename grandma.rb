95e252ee7161856aa26fe59984bc80498ac6a2e3
Yevgeniy Sobolev <yevgeniyerot@gmail.com>
2019-02-14 23:59:40 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,8 +16,8 @@ def speak_to_grandma(string)
   if(string == "I LOVE YOU GRANDMA!")
     return "I LOVE YOU TOO PUMPKIN!"
   elsif(string == string.upcase)
-    return "HUH?! SPEAK UP, SONNY!"
-  else
     return 'NO, NOT SINCE 1938!' 
+  else
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end
